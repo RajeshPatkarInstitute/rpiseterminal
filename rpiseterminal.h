d70f00f2a15e2aaa7f04a81cd31069644a8ab708
Rajesh Patkar <patkar@rajeshpatkar.com>
2021-11-02 18:33:46 +0530

    wait, teleport*

--- rpiseterminal.h
+++ rpiseterminal.h
@@ -9,6 +9,16 @@ enum Color
     cyan = '6',
     white = '7'
 };
+enum Attribute
+{
+    reset = '0',
+    bright = '1',
+    dim = '2',
+    underscore = '4',
+    blink = '5',
+    reverse = '7',
+    hidden = '8'
+};
 
 void clearScreen();
 void clearToLineEnd();
@@ -29,3 +39,9 @@ void showCursor();
 void hideCursor();
 int readKey(void);
 int readKeyInPlace(void);
+void setAttribute(Attribute ch);
+void wait(unsigned int secs);
+void teleportUp();
+void teleportDown();
+void teleportBack();
+void teleportAhead();
